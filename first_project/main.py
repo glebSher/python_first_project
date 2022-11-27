9ddc9d7b6a7a8d372650933e077b9a0e8f247c85
glebshcherbakov <gleb.sher309@gmail.com>
2022-11-27 14:52:52 +0300

    theory

--- first_project/main.py
+++ first_project/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+# a = 5
+# b = "string"
+# c = 0.5
+# t = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(t))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# for i in range(5):
+#     print("python")
 
+# i = 0
+# while i < 3:
+#     print("python")
+#     i += 1 # также можно умножить, вычесть, разделить: -=, *=, /=
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# n = 567
+# print(n // 10)
+# print(n % 10)
 
+# sp = []
+# sp.append(2)
+# sp.append(9)
+# print(sp)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+sp = [1, 2, 3, 5]
+# print(sp[2])
+# print(sp[-1]) # вывод последнего элемента в списке (нумерация с конца: -1, -2 и т.д.)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(0, len(sp)):
+    # print(sp[i])
+    print(sp[i], end=" ") # вывод по индексам в списке (если что-то меняется, то исп. этот способ)
+
+for el in sp:
+    print(el, end=" ") # вывод по элементам
+
+print(*sp) # без (*) выводит в кв.скобках, со (*) без них
+
